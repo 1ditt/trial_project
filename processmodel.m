0844e626161d0f18d290e01c836c6556fcb81cb7
vandit <74812342+1ditt@users.noreply.github.com>
2023-11-28 17:12:18 +0530

    Update processmodel.m

--- processmodel.m
+++ processmodel.m
@@ -38,7 +38,7 @@ function processmodel(pm)
     if includeModelStandardsTask
         maTask = pm.addTask(padv.builtin.task.RunModelStandards());
         maTask.addInputQueries(padv.builtin.query.FindFileWithAddress( ...
-            Type='ma_config_file', Path=fullfile('tools','sampleChecks.json1234')));
+            Type='ma_config_file', Path=fullfile('tools','sampleChecks11.json')));
         % Change Report path
         maTask.ReportPath = fullfile(...
             '$DEFAULTOUTPUTDIR$','$ITERATIONARTIFACT$','model_standards_results');
